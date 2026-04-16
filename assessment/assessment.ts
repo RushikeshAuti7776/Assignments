4ed7b7203552779a0287d5e37096b23251817d8f
unknown <rushikeshauti247@gmail.com>
2026-04-17 01:49:05 +0530

    added Assessment

---
+++ assessment/assessment.ts
@@ -0,0 +1,43 @@
+// Assessment Q1 Temperature of a city in degrees Celsius: 25.5
+let degree: number = 25.5;
+console.log(`Assessment Q1 Temperature of a city in degrees Celsius: ${degree}`)
+
+// Assessment Q2 Whether a customer has placed an order: true or false
+const i = 1 ;
+const j = 5 ;
+let result : string =(i >j) ? "true" : "false";
+
+let order : boolean = true || false ;
+console.log(`Assessment Q2 Whether a customer has placed an order:${order} or ${result}`);
+
+// Assessment Q3 Person's phone number: "123-456-7890"
+let number = "123-456-7890";
+console.log(`Person's phone number: "${number}"`)
+
+// Assessment Q4 Amount of money in a customer's bank account: 1000.50
+console.log(`--------------------------------------------------`)
+let money: number = 1000.50
+console.log(`Amount of money in a customer's bank account: ${number}`)
+
+// Assessment Q5 Person's email address: "john.doe@example.com"
+console.log(`--------------------------------------------------`)
+
+let email : string = "john.doe@example.com"
+console.log(`Person's email address: "${email}"`)
+
+// Assessment Q6 Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+console.log(`--------------------------------------------------`)
+let latitude : number = 37.7749
+let longitude : number = -122.4194
+console.log(`Coordinates of a location (latitude, longitude): ${latitude}, ${longitude}`)
+
+// Assessment Q7 Person's marital status: true or false
+console.log(`--------------------------------------------------`)
+let marital : boolean=true
+let no_marital : boolean =false
+console.log(`Person's marital status: ${marital},${no_marital}`)
+
+// Assessment Q8 Person's occupation: "Software Engineer"
+console.log(`--------------------------------------------------`)
+let occupation : string="Software Engineer"
+console.log(`Person's occupation:"${occupation}"`)
